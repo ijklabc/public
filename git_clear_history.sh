6022f1fe9317ba570214f5130caf5bdd0ab27623
wtangxyz <wtangxyz@gmail.com>
2019-11-03 22:23:49 +0800

    add files

---
+++ git_clear_history.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# https://tecadmin.net/delete-commit-history-in-github/
+git checkout --orphan temp_branch
+git add -A
+git commit -am "the first commit"
+git branch -D master
+git branch -m master
+git push -f origin master
